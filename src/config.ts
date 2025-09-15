85aaea65bbc32d988a5184fc68bf5e632a6df0fa
biggerperson <rats900@gmail.com>
2025-09-15 13:26:44 -0500

    take-two

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // src/config.ts
 // Keep tokens out of source in production; use secure storage/CI secrets
 export const MAPBOX_TOKEN = '<YOUR_MAPBOX_ACCESS_TOKEN>';
-export const MAPBOX_GEOCODING_ENDPOINT = 'https://api.mapbox.com/geocoding/v5/mapbox.places';
+export const MAPBOX_GEOCODING_ENDPOINT = 'https://api.mapbox.com/geocoding/v5/mapbox.places';  
